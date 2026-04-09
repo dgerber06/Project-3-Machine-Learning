d6abd6c29719faa4d77f6cffb28351400845881c
ShadowPikmin <jdcarter432@gmail.com>
2026-04-09 14:22:25 -0500

    Commits working implementation of Q6

--- machinelearning/models.py
+++ machinelearning/models.py
@@ -416,16 +416,18 @@ class Attention(Module):
         """YOUR CODE HERE"""
 
         K = self.k_layer(input)
-        Q = self.k_layer(input)
-        Q_trans = movedim(Q, 3, 2)
+        Q = self.q_layer(input)
         V = self.v_layer(input)
 
-        h1 = matmul(k, Q_trans)
-        d = torch.sqrt(self.layer_size)
+        Q_trans = movedim(Q,1,2)
+
+        h1 = matmul(K, Q_trans)
+        d = self.layer_size ** 0.5
 
         h2 = torch.div(h1, d)
-        M = M.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))[0]
+        M = h2.masked_fill(self.mask[:,:,:T,:T] == 0, float('-inf'))[0]
 
-        h3 = matmul(M, h2)
+        h3 = softmax(M, dim=-1)
+
         y_pred = matmul(h3, V)
         return y_pred
